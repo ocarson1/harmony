b23cba884bd8aeb20894152cedaac0a7f3e10ae6
vli18 <vivian_li1@brown.edu>
2022-12-03 17:34:11 -0500

    spotify auth is kinda working - still figuring it out

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,152 +1,3 @@
-// import React, { useEffect, useState, useRef } from 'react'
-// import Map, {    
-//   ViewState, ViewStateChangeEvent,
-//   MapLayerMouseEvent,
-//   Source, Layer, PointLike, MapRef, MapboxGeoJSONFeature } from 'react-map-gl' 
-// import 'mapbox-gl/dist/mapbox-gl.css'
-// import './App.css';
-// import {overlayData, geoLayer, isFeatureCollection} from './overlays'
-// import Redlining from './Redlining'
-// import {myKey} from './private/key'
-
-
-// // global variables representing the state, city, and name of location last clicked on
-// let requestedState: string = "";
-// let requestedCity: string = "";
-// let requestedName: string = "";
-
-// /**
-//  * App function that contains and sets up the React Components: Map and Redlining.
-//  * This also contains the functionality of handling a click event on the map and updating
-//  * the redlining properties.
-//  * @returns App function 
-//  */
-// function App() {
-//   const mapRef = useRef<MapRef>(null) // need types on this
-
-//   const [viewState, setViewState] = useState<ViewState>({
-//     longitude: -71.4129,
-//     latitude: 41.8245,
-//     zoom: 10,
-//     bearing: 0,
-//     pitch: 0,
-//     padding: {top: 1, bottom: 20, left: 1, right: 1}
-//   })
-
-//   const [overlay, setOverlay] = useState<GeoJSON.FeatureCollection | undefined>(undefined)
-
-//   // handles the clicking event on a certain area of the map
-//   const handleSubmit = (ev: MapLayerMouseEvent) => {
-//     console.log(ev.point.x + " " + ev.point.y)
-//     const bbox: [PointLike, PointLike] = [
-//       [ev.point.x - 5, ev.point.y - 5],
-//       [ev.point.x + 5, ev.point.y + 5]
-//      ]
-
-//      if (mapRef != null && mapRef.current != null) {
-//        const selectedFeatures: MapboxGeoJSONFeature[] = mapRef.current.queryRenderedFeatures(bbox, {})
-//        const fips = selectedFeatures.map((feature) => feature.properties)
-
-//        if (fips[0] != null) {
-//          console.log(fips[0].state)
-//          console.log(fips[0].city)
-//          console.log(fips[0].name)
-
-//          if(fips[0].state != null && fips[0].state != null) {
-//            requestedState = fips[0].state
-//            requestedCity = fips[0].city
-//            requestedName = fips[0].name
-//          }
-//          else {
-//            requestedState = "Not a redlining area"
-//            requestedCity = "Not a redlining area"
-//            requestedName = "Not a redlining area"
-//          }
-//        }
-//      }
-//    }
-
-//   // Run this _once_, and never refresh (empty dependency list)
-//   useEffect(() => {
-//     overlayData().then(data => {
-//       setOverlay(() => {
-//         if (isFeatureCollection(data)) {
-//           return data
-//         }
-
-//         return undefined
-//       })
-//     })
-//   }, [])
-
-//   // returning the HTML of the Map and Redlining React components
-//   return (
-//     <div className="App">
-//       <Map
-//          ref={mapRef}
-//          mapboxAccessToken={myKey}
-//          latitude={viewState.latitude}
-//          longitude={viewState.longitude}
-//          zoom={viewState.zoom}
-//          pitch={viewState.pitch}
-//          bearing={viewState.bearing}
-//          padding={viewState.padding}
-//          onMove={(ev: ViewStateChangeEvent) => setViewState(ev.viewState)} 
-//          onClick={handleSubmit}
-
-//          style={{width:'auto', height:3*(window.innerHeight/4), margin: 'auto'}}
-//          mapStyle={'mapbox://styles/mapbox/streets-v11'}>
-
-//           <Source id="geo_data" type="geojson" data={overlay}>
-//             <Layer {...geoLayer} />
-//           </Source>
-//         </Map>  
-//         <Redlining state={requestedState} city={requestedCity} name={requestedName}/>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-  // const navigate = useNavigate();
-
-  // const navigateLogin = () => {
-  //   // 👇️ navigate to /login
-  //   navigate('/login');
-  // };
-
-  // const navigateWebapp = () => {
-  //   // 👇️ navigate to /webapp
-  //   navigate('/webapp');
-  // };
-
-  // const openLogIn = () => {
-  //   return (
-  //     <div className="App">
-  //     <div className="web-container">
-  //       <img className="" src="./images/mapboxbackground.jpg"></img>
-  //       <LogIn></LogIn>
-  //     </div>
-  //   </div>
-  //   )
-  // }
-  // basic framework of first page
-//   return (
-//     <div className="App">
-//       <div className="web-container">
-//         <img className="" src="./images/mapboxbackground.jpg"></img>
-//         <button onClick={navigateLogin}>Log In</button>
-//         <button onClick={navigateWebapp}>Webapp</button>
-
-//         <Routes>
-//           <Route path="/login" element={<Login />} />
-//           <Route path="/" element={<Webapp />} />
-//         </Routes>
-//       </div>
-//     </div>
-//   );
-// }
-
 import React, { useEffect, useState, useRef } from 'react'
 // import {Routes, Route, useNavigate} from 'react-router-dom'
 import './App.css';
@@ -164,35 +15,41 @@ const getTokenFromUrl = () => {
 }
 
 function App() {
-  const [spotifyToken, setSpotifyToken] = useState("");
+  const CLIENT_ID = '50b02aae40fd4b2c99db1dbe69cb3935'
+  const REDIRECT_URI = 'http://localhost:3000'
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  const RESPONSE_TYPE = "token"
+
+  const [token, setToken] = useState("");
 
   useEffect(() => {
-    console.log("This is what was derived from the URL: ", getTokenFromUrl())
-    const spotifyToken = getTokenFromUrl()
-    window.location.hash= "";
-    console.log("This is our spotify token", spotifyToken);
+    const hash: string = window.location.hash
+    let token: string = window.localStorage.getItem("token")!
 
-    if (spotifyToken) {
-      setSpotifyToken(spotifyToken)
-    }
-  })
+    if (!token && hash) {
+      //idk why the types r inconsistent ts so annoying
+        let token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split('=')[1]
 
+        console.log(token)
+  }
+}, [])
+  // return (
+  //   <div className="App">
+  //     <div className="web-container">
+  //       <img className="" src="./images/mapboxbackground.jpg"></img>
+  //       <LogIn></LogIn>
+  //     </div>
+  //   </div>
+  // );
+
+  //temporary html for testing spotify auth from tutorial
   return (
     <div className="App">
-      <div className="web-container">
-        <img className="" src="./images/mapboxbackground.jpg"></img>
-        <LogIn></LogIn>
-      </div>
+       <header className="App-header">
+                <h1>Spotify React</h1>
+                <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
+        </header>
     </div>
   );
 }
-
-// function Login() {
-//   return <LogIn></LogIn>
-// }
-
-// function Webapp() {
-//   return <h2>Contacts</h2>;
-// }
-
 export default App;
