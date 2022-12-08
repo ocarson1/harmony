34a1821d10c1ded56bce26b0d8bfaba0cfc9769b
Grace Cantarella <53235652+grcecant@users.noreply.github.com>
2022-12-07 21:25:15 -0500

    trying to fix

--- backend/src/main/java/server/handlers/GetRecommendationHandler.java
+++ backend/src/main/java/server/handlers/GetRecommendationHandler.java
@@ -3,11 +3,7 @@ package server.handlers;
 import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.Moshi;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
