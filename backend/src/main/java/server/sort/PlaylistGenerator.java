df89cb32bf8d0bbd58dd7d0c26556102b98fc158
Arman Deendar <90534992+adeendar@users.noreply.github.com>
2022-12-11 18:42:19 -0500

    Update PlaylistGenerator.java

--- backend/src/main/java/server/sort/PlaylistGenerator.java
+++ backend/src/main/java/server/sort/PlaylistGenerator.java
@@ -1,20 +1,3 @@
-package server.sort;
-
-import java.util.List;
-import server.deserializationObjects.RecommendationObj.ID;
-
-public class PlaylistGenerator {
-
-  private List<ID> tracks;
-
-  public PlaylistGenerator(List<ID> tracks){
-    this.tracks = tracks;
-  }
 
 
 
-
-
-
-
-}
