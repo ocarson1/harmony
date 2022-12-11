4d92a409351d1c2f881bcc5533de8b2cb3e7f9b3
ocarson1 <owen_carson@brown.edu>
2022-12-11 12:16:04 -0500

    working on markers

---
+++ client/src/Markers.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Marker } from "react-map-gl";
+import mockSongs from './mockData/mockSongs.json'
+//import { useStateMap } from "../hooks/mapHook";
+export const Markers = () => {
+    const songdata = mockSongs;
+    for (var key in songdata.jsonData)
+
+
+    }
+
+    
+  //const { markers } = useStateMap();
+  return (
+    
+      
+        <Marker
+          latitude={12}
+          longitude={12}
+          scale={.1}
+       >
+         <img src="https://i.scdn.co/image/ab67616d0000b27304b052e84325a16bc18a4c78" />
+        </Marker>
+      
+    
+  );
+};
