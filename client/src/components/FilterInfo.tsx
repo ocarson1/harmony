02558b3bfafbbbc3093885cfe0dcf0a490f8eb12
alippman2002 <98556660+alippman2002@users.noreply.github.com>
2022-12-18 20:44:54 -0600

    year and genre reset

--- client/src/components/FilterInfo.tsx
+++ client/src/components/FilterInfo.tsx
@@ -1,35 +1,38 @@
-
 import '../styles/FilterInfo.css'
 import Dropdown from 'react-dropdown'
 import React, {useState} from 'react'
+
 interface filterInfoProps {
-    categories: Array<Set<string>>
-    setFilter: Function
+  categories: Array<Set<string>>
+  setFilter: Function
 }
 
 export default function FilterInfo(props: filterInfoProps) {
+  const [selectedCategory1, setSelectedCategory1] = useState('Year');
+  const [selectedCategory2, setSelectedCategory2] = useState('Genre');
 
-    const category1 = 'Year'
-    let options1 = new Array
-    const category2 = 'Genre'
-    let options2 = new Array
-    
-    if (props.categories.length != 0) {
-        options1 = Array.from(props.categories[0])
-        options1 = options1.sort(function(a,b){return a-b})
-        options2 = Array.from(props.categories[1])
-        options2 = options2.sort()
-    }
+  const options1 = Array.from(props.categories[0]).sort(function(a: any,b: any){
+    return a - b;
+  });
+  const options2 = Array.from(props.categories[1]).sort();
 
-return (
+  function reset() {
+    setSelectedCategory1('Year');
+    setSelectedCategory2('Genre');
+    props.setFilter([]);
+  }
+
+  return (
     <div className="filter-info">
-        <button className="reset-button" onClick ={() => props.setFilter(new Map)}>Reset Filters</button>
-        <Dropdown options={options1} value={category1} onChange={(e) => {
-            props.setFilter({"release_date" : e.value})
-        }}/>
-        <Dropdown options={options2} value = {category2} onChange={(e) => {
-            props.setFilter({"genres" : e.value})
-        }}/>
+      <button className="reset-button" onClick={() => reset()}>Reset Filters</button>
+      <Dropdown options={options1} value={selectedCategory1} onChange={(e) => {
+        setSelectedCategory1(e.value);
+        props.setFilter({"release_date" : e.value});
+      }}/>
+      <Dropdown options={options2} value={selectedCategory2} onChange={(e) => {
+        setSelectedCategory2(e.value);
+        props.setFilter({"genres" : e.value});
+      }}/>
     </div>
-)
+  )
 }
